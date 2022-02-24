cc2921fd82b5d87f0d6fe2860748f2b77f1b860d
Addison Hernandez <addison.hernandez@gmail.com>
2022-02-24 16:58:53 -0500

    Implement ES6 stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.head = 0;
+    this.tail = -1;
+  }
+
+  enqueue(value) {
+    this.tail++;
+    this[this.tail] = value;
+  }
+
+  dequeue() {
+    var hasItems = this.size() > 0;
+    if (hasItems) {
+      var deletedItem = this[this.head];
+      delete this[this.head];
+      this.head++;
+      return deletedItem;
+    }
+  }
+
+  size() {
+    return this.tail + 1 - this.head;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,36 @@
+// Do:
+//  - Work within the src/es6/ folder
+//  - Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+//  - Use the keyword new when instantiating your class
+//  - Use the keyword this in your constructor
+//  - Explicitly declare a class method named constructor
+//  - Declare all other class methods within the class declaration
+// Don't:
+//  - Declare the instance explicitly
+//  - Return the instance explicitly
+//  - Add class methods to the class prototype directly
+
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.top = -1;
   }
 
+  push(value) {
+    this[++this.top] = value;
+  }
+
+  pop() {
+    if (this.size() > 0) {
+      var deletedItem = this[this.top];
+      delete this[this.top--];
+      return deletedItem;
+    }
+  }
+
+  size() { return this.top + 1; }
+
 }
+
+var myStack = new Stack();
