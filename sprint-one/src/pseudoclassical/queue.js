778f15d5485e2065d225996a67dd63df76f72d89
Addison Hernandez <addison.hernandez@gmail.com>
2022-02-24 16:58:26 -0500

    Implement pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,35 @@
+// Do:
+//  - Work within the src/pseudoclassical/ folder
+//  - Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+//  - Use the keyword new when instantiating your class
+//  - Use the keyword this in your constructor
+// Don't:
+//  - Declare the instance explicitly
+//  - Return the instance explicitly
+
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.head = 0;
+  this.tail = -1;
 };
 
+Queue.prototype.enqueue = function (value) {
+  this.tail++;
+  this[this.tail] = value;
+};
 
+Queue.prototype.dequeue = function () {
+  var hasItems = this.size() > 0;
+
+  if (hasItems) {
+    var deletedItem = this[this.head];
+    delete this[this.head];
+    this.head++;
+    return deletedItem;
+  }
+};
+
+Queue.prototype.size = function () {
+  return this.tail - this.head + 1;
+};
+
+var myQueue = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,36 @@
+// Do:
+//  - Work within the src/pseudoclassical/ folder
+//  - Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+//  - Use the keyword new when instantiating your class
+//  - Use the keyword this in your constructor
+// Don't:
+//  - Declare the instance explicitly
+//  - Return the instance explicitly
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.top = -1;
 };
 
+// Add methods to Stack.prototype
+Stack.prototype.push = function(value) {
+  this.top++;
+  this[this.top] = value;
+};
 
+Stack.prototype.pop = function() {
+  var hasItems = this.size() > 0;
+  if (hasItems) {
+    var deletedItem = this[this.top];
+    delete this[this.top];
+    this.top--;
+    return deletedItem;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.top + 1;
+};
+
+var myStack = new Stack();
