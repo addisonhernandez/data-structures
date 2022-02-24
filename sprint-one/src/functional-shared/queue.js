7258c92f764b90811b47530c3911b40fe0299513
Addison Hernandez <addison.hernandez@gmail.com>
2022-02-24 16:56:46 -0500

    Implement functional-shared stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,44 @@
+// Do:
+//  - Work within the src/functional-shared/ folder
+//  - Create an object that holds the methods that will be shared by all instances of the class
+//  - Use the keyword `this` in your methods
+//  - Use _.extend  to copy the methods onto the instance
+// Don't:
+//  - Use the keyword new or prototype chains
+
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {
+    head: 0,
+    tail: -1
+  };
+
+  _.extend(storage, queueMethods);
+
+  return storage;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue: function (value) {
+    this.tail++;
+    this[this.tail] = value;
+  },
+
+  dequeue: function () {
+    if (this.size() > 0) {
+      var deletedItem = this[this.head];
+      delete this[this.head];
+      this.head++;
+      return deletedItem;
+    }
+  },
+
+  size: function () {
+    return this.tail + 1 - this.head;
+  }
+
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,47 @@
+// Do:
+//  - Work within the src/functional-shared/ folder
+//  - Create an object that holds the methods that will be shared by all instances of the class
+//  - Use the keyword `this` in your methods
+//  - Use _.extend  to copy the methods onto the instance
+// Don't:
+//  - Use the keyword new or prototype chains
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // create storage object
+  var storage = {
+    top: -1
+  };
+  _.extend(storage, stackMethods);
+  // return object
+  return storage;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  // push
+  push: function(value) {
+    this.top++;
+    this[this.top] = value;
+  },
 
+  // pop
+  pop: function() {
+    if (this.size() > 0) {
+      var deletedValue = this[this.top];
+      delete this[this.top];
+      this.top--;
+      return deletedValue;
+    }
+  },
 
+  // size
+  size: function() {
+    return this.top + 1;
+  }
+};
+
+// create an instance of Stack
+
+// extend : stackMethods ~> stack instance
