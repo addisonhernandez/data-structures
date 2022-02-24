2415d109b9672094e3b1651eb5e976c4fe07cc01
Addison Hernandez <addison.hernandez@gmail.com>
2022-02-24 16:57:47 -0500

    Implement prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = Object.create(queueMethods);
+
+  storage.head = 0;
+  storage.tail = -1;
+
+  return storage;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.tail++;
+    this[this.tail] = value;
+  },
+
+  dequeue: function() {
+    var hasItems = (this.tail + 1 - this.head) > 0;
+    if (hasItems) {
+      var deletedItem = this[this.head];
+      delete this[this.head];
+      this.head++;
+      return deletedItem;
+    }
+  },
+  size: function() {
+    return this.tail + 1 - this.head;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Object.create an instance
+  var storage = Object.create(stackMethods);
+
+  // decorate with `top` property
+  storage.top = -1;
+
+  // return
+  return storage;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function (value) {
+    this.top++;
+    this[this.top] = value;
+  },
+
+  pop: function () {
+    var hasItems = this.top >= 0;
+
+    if (hasItems) {
+      var deletedItem = this[this.top];
+      delete this[this.top];
+      this.top--;
+      return deletedItem;
+    }
+  },
+
+  size: function () {
+    return this.top + 1;
+  }
+};
 
 
