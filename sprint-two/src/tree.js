6eeb6735edee5644fb4e633c818c69c81d515f98
Addison Hernandez <addison.hernandez@gmail.com>
2022-02-25 12:28:51 -0500

    Implement Tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,45 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 var treeMethods = {};
 
+/**
+ * Adds a new value as a subtree
+ * @param {*} value Value to store in the new sub-tree
+ */
 treeMethods.addChild = function(value) {
+  // make a new `Tree`
+  var child = Tree(value);
+  // push the newTree to the children prop
+  this.children.push(child);
 };
 
+/**
+ * Takes any input and returns whether it can be found as the value of the target node or any descendant node
+ * @param {*} target - value to search for in the current tree
+ * @returns {Boolean} `true` if the tree contains `target`, `false` otherwise
+ */
 treeMethods.contains = function(target) {
+  // inner recursive search
+  var searchChild = function (childTree) {
+    //  if value == target -> true
+    if (childTree.value === target) {
+      return true;
+    }
+    //  if value != target ->
+    //      _.each: recursively search each child
+    return _.some(childTree.children, function(child) {
+      return searchChild(child);
+    });
+  };
+
+  return searchChild(this);
 };
 
 
@@ -21,3 +49,8 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// addChild:
+// O(1)
+
+// contains:
+// O(n)
