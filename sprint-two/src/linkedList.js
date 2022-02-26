1bf76dc50884751aae4bc3bcebb15323af0670fb
Addison Hernandez <addison.hernandez@gmail.com>
2022-02-26 14:55:34 -0500

    Remove pseudocode comments

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -12,29 +12,14 @@ var LinkedList = function() {
    * @param {*} value - The item to be added to the end of the list
    */
   list.addToTail = function(value) {
-    //  H         T
-    // [ ]->[ ]->[ ]->null
-
-    //  H         T    N        (N is for new node)
-    // [ ]->[ ]->[ ]->[ ]->null
-
-    //  H              T/N
-    // [ ]->[ ]->[ ]->[ ]->null
-
-
-    // make a new node
-    //  store `value` in the new node
     const newNode = Node(value);
 
-    // Edge case: what if the list is empty
+    // Edge case: the list is empty
     if (list.head === null) {
       list.head = newNode;
       list.tail = newNode;
     } else {
-      // update tail.next to point to our new node
       list.tail.next = newNode;
-
-      // make the new node the tail of the list
       list.tail = newNode;
     }
   };
@@ -44,24 +29,17 @@ var LinkedList = function() {
    * @returns The value of the removed node
    */
   list.removeHead = function() {
-    // Edge case: what if the list is empty
-    // - what do we return? -> null
+    // Edge case: the list is empty
     if (list.head === null) {
       return null;
     }
-
-    // list.head (Node)
-    // - value
-    // - next -> (Node)
 
     var deletedValue = list.head.value;
 
     if (list.head.next) {
       list.head = list.head.next;
     } else {
-      // Edge case: what if there's only one element?
-      //  T     H
-      // [ ]-> null
+      // there is no next node -> retore initial state
       list.head = null;
       list.tail = null;
     }
@@ -75,17 +53,6 @@ var LinkedList = function() {
    * @returns {Boolean} returns boolean reflecting whether or not the passed-in value is in the linked list
    */
   list.contains = function(target) {
-    // use `arguments` to see if a node has already been passed in
-    // if not ~> current node := head
-
-    // if target === current node.value => return true
-
-    // if current node has a next:
-
-    //  recursively call: contains(target, current Node.next)
-    // if current node does not have next:
-    //  return false;
-
     var currentNode = list.head;
     while (currentNode) {
       if (target === currentNode.value) {
