7f4e080f479f7de89ffc0fd4d64da0d2f1c85d0b
Addison Hernandez <addison.hernandez@gmail.com>
2022-02-26 15:05:15 -0500

    Remove pseudocode comments

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -16,9 +16,7 @@ var treeMethods = {};
  * @param {*} value Value to store in the new sub-tree
  */
 treeMethods.addChild = function(value) {
-  // make a new `Tree`
   var child = Tree(value);
-  // push the newTree to the children prop
   this.children.push(child);
 };
 
@@ -28,14 +26,11 @@ treeMethods.addChild = function(value) {
  * @returns {Boolean} `true` if the tree contains `target`, `false` otherwise
  */
 treeMethods.contains = function(target) {
-  // inner recursive search
   var searchChild = function (childTree) {
-    //  if value == target -> true
     if (childTree.value === target) {
       return true;
     }
-    //  if value != target ->
-    //      _.each: recursively search each child
+
     return _.some(childTree.children, function(child) {
       return searchChild(child);
     });
