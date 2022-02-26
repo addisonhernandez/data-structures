f87e480d6d3dbd889a2b4cfa1868bcf1e8bd40af
Addison Hernandez <addison.hernandez@gmail.com>
2022-02-26 13:21:27 -0500

    Implement hash table and pass basic tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,17 +4,40 @@ var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
+var ht = new HashTable();
+console.log(ht);
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  // edge case: what if _storage[index] is already occupied?
+  // should we always store an array?
+
+  var itemsInBucket = this._storage.get(index) || {};
+
+  itemsInBucket[k] = v;
+
+  this._storage.set(index, itemsInBucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var itemsInBucket = this._storage.get(index);
+
+  if (!itemsInBucket) {
+    return undefined;
+  }
+
+  return itemsInBucket[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var itemsInBucket = this._storage.get(index);
+
+  delete itemsInBucket[k];
 };
 
 
